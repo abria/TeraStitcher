544cc31f964394d39c83c36d828cdb52452d8f81
Alessandro Bria <a.bria@unicas.it>
2016-04-15 17:52:55 +0200

    added pack/bundle step in the CMake process

--- src/common/config.cpp
+++ src/common/config.cpp
@@ -13,7 +13,7 @@ namespace terastitcher
     *    PARAMETERS    *
     ********************
     ---------------------------------------------------------------------------------------------------------------------------*/
-    std::string version = "1.9.5";
+	std::string version = num2str<int>(TERASTITCHER_MAJOR) + "." + num2str<int>(TERASTITCHER_MINOR) + "." + num2str<int>(TERASTITCHER_PATCH);
     int DEBUG = NO_DEBUG;                    //debug level
 #ifdef WITH_QT
 	std::string qtversion = QT_VERSION_STR;
---
+++ src/cpack/README.txt
@@ -0,0 +1,3 @@
+Before using the tools contained in the /bin folder, you MUST accept the license (LICENSE.pdf).
+
+Documentation, help and other info are available on our Github wiki at http://abria.github.io/TeraStitcher/.
