139e2017d1f2a88915bd7ac15ab2ee3e37414378
Alessandro Bria <a.bria@unicas.it>
2017-02-03 11:09:22 +0100

    fixed TeraStitcher2 compilation issue / added VERSION numbers for TeraConverter and TeraStitcher2

--- src/common/config.cpp
+++ src/common/config.cpp
@@ -14,8 +14,8 @@ namespace terastitcher
     ********************
     ---------------------------------------------------------------------------------------------------------------------------*/
 	std::string version = num2str<int>(TERASTITCHER_MAJOR) + "." + num2str<int>(TERASTITCHER_MINOR) + "." + num2str<int>(TERASTITCHER_PATCH);
-	std::string teraconverter_version     = "3.1.0";
-	std::string terastitcher2_version     = "1.0.0";
+	std::string teraconverter_version     =  num2str<int>(TERACONVERTER_MAJOR) + "." + num2str<int>(TERACONVERTER_MINOR) + "." + num2str<int>(TERACONVERTER_PATCH);
+	std::string terastitcher2_version     = num2str<int>(TERASTITCHER2_MAJOR) + "." + num2str<int>(TERASTITCHER2_MINOR) + "." + num2str<int>(TERASTITCHER2_PATCH);
 	std::string mdatagenerator_version    = "1.0.0";
 	std::string mergedisplacement_version = "1.0.0";
     int DEBUG = NO_DEBUG;                    //debug level
