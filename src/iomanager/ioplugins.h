db45ef197e24fb08cc9fac21c78a80b2e1752e89
iannellog <g.iannello@unicampus.it>
2017-10-05 19:29:23 +0200

    Minor change

--- src/iomanager/ioplugins.h
+++ src/iomanager/ioplugins.h
@@ -1,8 +1,3 @@
 // Include plugins headers
-#include "plugins/IMS_HDF5/IMS_HDF5.h"
-<<<<<<< HEAD
-#include "plugins/opencv2D/opencv2D.h"
-=======
->>>>>>> 06d3968e0351c9c2858e8c76a2b363840957746c
 #include "plugins/tiff2D/tiff2D.h"
 #include "plugins/tiff3D/tiff3D.h"
