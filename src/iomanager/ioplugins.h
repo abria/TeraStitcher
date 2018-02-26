d182f5c66c3b52a0b5d6d9a27f81334eb95402f1
iannellog <g.iannello@unicampus.it>
2018-02-26 16:59:24 +0100

    Minor bugs in CMake files

--- src/iomanager/ioplugins.h
+++ src/iomanager/ioplugins.h
@@ -1,5 +1,6 @@
 // Include plugins headers
 #include "plugins/dcimg/dcimg.h"
 #include "plugins/IMS_HDF5/IMS_HDF5.h"
+#include "plugins/opencv2D/opencv2D.h"
 #include "plugins/tiff2D/tiff2D.h"
 #include "plugins/tiff3D/tiff3D.h"
